4bcbc470be50f4dacca4df172fd8dc5b3ccec968
victory-clan <46355716+victory-clan@users.noreply.github.com>
2019-01-29 19:53:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,31 +35,4 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
   }
  });
  
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-~~
-Sarcasm Land. يرحب بكم :dove:
-~~
-:kissing::heart::eyes: حيااكْ الله  فى سيرفرنا 
-╔╦╦╦═╦╗╔═╦═╦══╦═╗
-║║║║╩╣╚╣═╣║║║║║╩╣
-╚══╩═╩═╩═╩═╩╩╩╩═╝ 
-
-‏نقدم لك دعوة لسموك الراقيَ .
-‏لنحظى بجمال حضورك وتواجدك
-•بتنـورونـا الجميع :wink:
-•اي مشكله توآجهك ولا يهمك. عندك الاداره تفضل ونحل مشكلتك مع آي شخص بالسيرفر.:telephone_receiver:
-•عندك فكره موضوع تفضل شآركنا فيه:jack_o_lantern::gift:
-•واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.
-شرفنا يا عسل:wink: :kissing_heart:
- [https://discord.gg/h8RtRKM] 
-joined info-selfie
-Sarcasm Land.
-الدعووة خاصة لك ياقلبي ... [ ${member}  ]
-**`) 
-}).catch(console.error)
-})
-
 client.login(process.env.BOT_TOKEN);
