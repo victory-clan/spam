490bb49975d11d960387908d93c610eae379a261
victory-clan <46355716+victory-clan@users.noreply.github.com>
2019-01-29 16:16:39 +0200

    Add files via upload

---
+++ Sharing-BOT.js
@@ -0,0 +1,38 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+ 
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+~~
+Sarcasm Land. يرحب بكم :dove:
+~~
+:kissing::heart::eyes: حيااكْ الله  فى سيرفرنا 
+╔╦╦╦═╦╗╔═╦═╦══╦═╗
+║║║║╩╣╚╣═╣║║║║║╩╣
+╚══╩═╩═╩═╩═╩╩╩╩═╝ 
+
+‏نقدم لك دعوة لسموك الراقيَ .
+‏لنحظى بجمال حضورك وتواجدك
+•بتنـورونـا الجميع :wink:
+•اي مشكله توآجهك ولا يهمك. عندك الاداره تفضل ونحل مشكلتك مع آي شخص بالسيرفر.:telephone_receiver:
+•عندك فكره موضوع تفضل شآركنا فيه:jack_o_lantern::gift:
+•واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.
+شرفنا يا عسل:wink: :kissing_heart:
+ [https://discord.gg/h8RtRKM] 
+joined info-selfie
+Sarcasm Land.
+الدعووة خاصة لك ياقلبي ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+
+client.login(process.env.BOT_TOKEN);
+
+//A7MD.login('التوكين حقك');
